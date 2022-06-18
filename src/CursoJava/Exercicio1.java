f326e19286d1d9a65c59af19bccb05765d79be60
anafernanndess <analiuza123@gmail.com>
2022-06-18 17:22:30 -0300

    Exerciocio 1

---
+++ src/CursoJava/Exercicio1.java
@@ -0,0 +1,21 @@
+package CursoJava;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double a1, a2,a3;
+        Scanner entrada =  new Scanner(System.in);
+
+        a1 = 1.58;
+
+        a2 = 2.07;
+
+        a3 = 0.55;
+
+        double media = (a1 + a2 + a3) / 3;
+
+
+        System.out.println(media);
+    }
+}
